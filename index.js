ce92beaedfbf22ddad237ebe6720c3e355875f65
Xiao <49285022+xiao-title@users.noreply.github.com>
2021-12-17 01:07:02 +0900

    test

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
-const app = express();
 
+const app = express();
 app.use(cors());
 app.options("*", cors());
 
